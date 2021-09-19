dc31b9ca59f59872dd58892c57351a2b2f3c9bf5
Vuong <vuong@11.com>
2021-09-19 22:30:55 +0700

    layout1-2

---
+++ app/src/main/java/com/example/motionlayout_android/activity_ex2.java
@@ -0,0 +1,14 @@
+package com.example.motionlayout_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_ex2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex2);
+    }
+}
